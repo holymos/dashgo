70e877047d9fef46af129e8e646a3039f14ed9a4
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:58:12 -0300

    feat: add theme and global css

---
+++ src/styles/theme.js
@@ -0,0 +1,28 @@
+module.exports = {
+  colors: {
+    gray: {
+      50: "#EEEEF2",
+      100: "#D1D2DC",
+      200: "#B3B5C6",
+      300: "#9699B0",
+      400: "#797D9A",
+      500: "#616480",
+      600: "#4B4D63",
+      700: "#353646",
+      800: "#1F2029",
+      900: "#181B23",
+    },
+  },
+  maxWidth: {
+    360: "360px",
+    400: "400px",
+    1480: "1480px",
+  },
+  borderWidth: {
+    1: "1px",
+  },
+  gridTemplateColumns: {
+    "min-child-320": "repeat(auto-fit, minmax(320px, 1fr))",
+    "min-child-240": "repeat(auto-fit, minmax(240px, 1fr))",
+  },
+};
