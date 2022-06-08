ae5b98673245cdb68b3b1d1c28893888f46aa810
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:53:17 -0300

    feat: add NavLink atom component

---
+++ src/components/Atoms/NavLink/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+
+type NavLinkProps = {
+  icon: ReactElement;
+  description: string;
+  link?: string;
+};
+
+export const NavLink: React.FC<NavLinkProps> = ({
+  icon,
+  description,
+  link = "/",
+}) => {
+  return (
+    <Link href={link}>
+      <a className="flex items-center hover:text-gray-200">
+        {icon}
+        <span className="ml-4 font-medium">{description}</span>
+      </a>
+    </Link>
+  );
+};
