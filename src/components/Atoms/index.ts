1c200102a5b613ca1e29f728ca66e4f0657829fb
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:58:49 -0300

    feat: add Atoms, Molecules and Organisms export file

---
+++ src/components/Atoms/index.ts
@@ -0,0 +1,7 @@
+export { Logo } from "./Logo";
+export { Profile } from "./Profile";
+export { SearchBox } from "./SearchBox";
+export { NotificationsNav } from "./NotificationNav";
+export { PaginationItem } from "./PaginationItem";
+export { NavLink } from "./NavLink";
+export { Input } from "./Input";
