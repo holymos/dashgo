972d2ea0935b4d03bd0b408c1529c1348786b61d
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:57:23 -0300

    test: add tests for Header organism component

---
+++ src/components/Organisms/Header/test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { Header } from ".";
+
+describe("<Header />", () => {
+  it("should render correctly", () => {
+    const { container } = render(<Header />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
