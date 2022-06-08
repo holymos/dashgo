b66b4387f5c037f0d9031cee7098e35341706f27
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:52:45 -0300

    feat: add Logo atom component

---
+++ src/components/Atoms/Logo/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <a>
+        <p className="text-3xl font-bold tracking-tight w-64">
+          dashgo<span className="text-pink-500">.</span>
+        </p>
+      </a>
+    </Link>
+  );
+};
