47a73ab4e2f7c5987704a25a87148ab00259eeb9
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:55:17 -0300

    feat: add SearchBox atom component

---
+++ src/components/Atoms/SearchBox/index.tsx
@@ -0,0 +1,17 @@
+import { RiSearchLine } from "react-icons/ri";
+import theme from "../../../styles/theme";
+
+export const SearchBox = () => {
+  return (
+    <label className="flex flex-1 py-4 px-8 ml-6 max-w-400 self-center text-gray-200 relative bg-gray-800 rounded-full justify-between items-center">
+      <input
+        type="text"
+        aria-label="search-box"
+        placeholder="Buscar na plataforma"
+        className="text-gray-50 bg-transparent hover:bg-gray-800 border-none focus:ring-0 placeholder:text-gray-500 px-4 mr-4 h-0"
+      />
+
+      <RiSearchLine cursor="pointer" color={theme.colors.gray[500]} />
+    </label>
+  );
+};
