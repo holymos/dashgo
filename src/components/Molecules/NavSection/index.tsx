410e9450953c50840dda87cedd7589bff7781139
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:55:50 -0300

    feat: add NavSection molecule component

---
+++ src/components/Molecules/NavSection/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type NavSectionProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export const NavSection: React.FC<NavSectionProps> = ({ title, children }) => {
+  return (
+    <nav>
+      <p className="font-bold text-gray-400 text-xs">{title}</p>
+      <div className="flex flex-col gap-4 mt-8 items-stretch">{children}</div>
+    </nav>
+  );
+};
