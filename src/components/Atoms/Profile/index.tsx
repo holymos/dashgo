de41978f23e3b1683f4af535bec5a56a8a14cbef
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:54:53 -0300

    feat: add Profile atom component

---
+++ src/components/Atoms/Profile/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export const Profile = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        <div className="mr-4 text-right">
+          <p>Moisés Pacífico</p>
+          <p className="text-gray-300 text-sm">moises.pacifico@hotmail.com</p>
+        </div>
+
+        <div className="h-12 w-12 relative">
+          <Image
+            src="https://github.com/holymos.png"
+            alt="User image"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
