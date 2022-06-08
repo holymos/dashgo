5d476edd312ce910e5add674b87570905db522b1
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-08 20:57:39 -0300

    feat: add Sidebar organism component

---
+++ src/components/Organisms/Sidebar/index.tsx
@@ -0,0 +1,38 @@
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+import { NavLink } from "../../Atoms";
+import { NavSection } from "../../Molecules";
+
+export const Sidebar = () => {
+  return (
+    <aside className="w-64 mr-8">
+      <div className="flex flex-col gap-12 items-start">
+        <NavSection title="GERAL">
+          <NavLink
+            description="Dashboard"
+            icon={<RiDashboardLine fontSize={20} />}
+          />
+          <NavLink
+            description="Usuários"
+            icon={<RiContactsLine fontSize={20} />}
+          />
+        </NavSection>
+
+        <NavSection title="AUTOMAÇÃO">
+          <NavLink
+            description="Formulários"
+            icon={<RiInputMethodLine fontSize={20} />}
+          />
+          <NavLink
+            description="Automação"
+            icon={<RiGitMergeLine fontSize={20} />}
+          />
+        </NavSection>
+      </div>
+    </aside>
+  );
+};
