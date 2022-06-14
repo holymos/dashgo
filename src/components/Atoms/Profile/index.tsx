e9d94c381ce0626aaa0ab81b551661b44d3eb438
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-14 20:54:23 -0300

    refactor: set image size based on screen size

--- src/components/Atoms/Profile/index.tsx
+++ src/components/Atoms/Profile/index.tsx
@@ -1,22 +1,24 @@
 import Image from "next/image";
+import { useWindowSize } from "../../../hooks/useWindowSize";
 
 export const Profile = () => {
+  const { isLg, is2xl } = useWindowSize();
+  const imageSize = isLg || is2xl ? 38 : 34;
+
   return (
     <div className="flex items-center">
-      <div className="hidden md:block mr-4 text-right">
+      <div className="hidden md:block mr-2 md:mr-4 text-right">
         <p>Moisés Pacífico</p>
         <p className="text-gray-300 text-sm">moises.pacifico@hotmail.com</p>
       </div>
 
-      <div className="h-12 w-12 relative">
-        <Image
-          src="https://github.com/holymos.png"
-          alt="User image"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-full"
-        />
-      </div>
+      <Image
+        src="https://github.com/holymos.png"
+        alt="User image"
+        width={imageSize}
+        height={imageSize}
+        className="rounded-full"
+      />
     </div>
   );
 };
