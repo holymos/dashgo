c661c526264cbd20e699bac3d11fa40444d9907f
Moisés Pacífico <moises.pacifico@hotmail.com>
2022-06-17 18:26:30 -0300

    feat: add react-hook-form

--- src/components/Atoms/Input/index.tsx
+++ src/components/Atoms/Input/index.tsx
@@ -1,16 +1,39 @@
+import { FieldError, UseFormRegister } from "react-hook-form";
+
 type InputProps = {
   name: string;
   label: string;
   type?: string;
+  register: UseFormRegister<{ [key: string]: string }>;
+  required?: boolean;
+  error: FieldError;
 };
 
-export const Input: React.FC<InputProps> = ({ label, name, type = "text" }) => {
+export const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  register,
+  required = false,
+  error,
+  type = "text",
+}) => {
+  console.log({ error });
+
   return (
     <div className="flex flex-col space-y-2">
-      <label htmlFor={name} className="text-gray-500">
+      <label htmlFor={name} id={name} className="text-gray-500">
         {label}
       </label>
-      <input type={type} id={name} name={name} />
+      <input
+        type={type}
+        id={name}
+        name={name}
+        {...(register(name),
+        {
+          required,
+        })}
+      />
+      {error && <span>{error.message}</span>}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Atoms/Input";
 
+type FormInput = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<FormInput> = (data) => {
+    console.log({ data });
+  };
+
   return (
     <div className="flex w-screen h-screen justify-center items-center">
-      <form className="flex flex-col space-y-4 w-full max-w-360 bg-gray-800 p-8 rounded-lg">
-        <Input name="email" label="Email" type="email" />
-        <Input name="password" label="Senha" type="password" />
+      <form
+        className="flex flex-col space-y-4 w-full max-w-360 bg-gray-800 p-8 rounded-lg"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Input
+          name="email"
+          label="Email"
+          type="email"
+          register={register}
+          error={errors.email}
+          required
+        />
+        <Input
+          name="password"
+          label="Senha"
+          type="password"
+          register={register}
+          error={errors.password}
+          required
+        />
 
         <button type="submit" className="mt-6 bg-pink-500 p-3 rounded-lg">
           Entrar
